0c583fd4162766ef5ebde856058d1c2ea796a06e
Halverine <150096220+Halverine@users.noreply.github.com>
2025-04-22 12:34:13 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,121 @@
+// External JavaScript file
+const button1 = document.getElementById("Task10");
+
+button1.addEventListener("click", function() {
+    alert("Well Done!");
+    button1.style.display = 'none';
+});
+const button2 = document.getElementById("Task20");
+
+button2.addEventListener("click", function() {
+    alert("Well Done!");
+    button2.style.display = 'none';
+});
+const button3 = document.getElementById("Task30");
+
+button3.addEventListener("click", function() {
+    alert("Well Done!");
+    button3.style.display = 'none';
+});
+const button4 = document.getElementById("Task40");
+
+button4.addEventListener("click", function() {
+    alert("Well Done!");
+    button4.style.display = 'none';
+});
+const button5 = document.getElementById("Task50");
+
+button5.addEventListener("click", function() {
+    alert("Well Done!");
+    button5.style.display = 'none';
+});
+const button6 = document.getElementById("Task60");
+
+button6.addEventListener("click", function() {
+    alert("Well Done!");
+    button6.style.display = 'none';
+});
+const button = document.getElementById("Task70");
+
+button7.addEventListener("click", function() {
+    alert("Well Done!");
+    button7.style.display = 'none';
+});
+const button8 = document.getElementById("Task80");
+
+button8.addEventListener("click", function() {
+    alert("Well Done!");
+    button8.style.display = 'none';
+});
+const button9 = document.getElementById("Task90");
+
+button9.addEventListener("click", function() {
+    alert("Well Done!");
+    button9.style.display = 'none';
+});
+const button10 = document.getElementById("Task100");
+
+button10.addEventListener("click", function() {
+    alert("Well Done!");
+    button10.style.display = 'none';
+});
+const button11 = document.getElementById("Task110");
+
+button11.addEventListener("click", function() {
+    alert("Well Done!");
+    button11.style.display = 'none';
+});
+const button12 = document.getElementById("Task120");
+
+button12.addEventListener("click", function() {
+    alert("Well Done!");
+    button12.style.display = 'none';
+});
+const button13 = document.getElementById("Task130");
+
+button13.addEventListener("click", function() {
+    alert("Well Done!");
+    button13.style.display = 'none';
+});
+const button14 = document.getElementById("Task140");
+
+button14.addEventListener("click", function() {
+    alert("Well Done!");
+    button14.style.display = 'none';
+});
+const button15 = document.getElementById("Task150");
+
+button15.addEventListener("click", function() {
+    alert("Well Done!");
+    button15.style.display = 'none';
+});
+const button16 = document.getElementById("Task160");
+
+button16.addEventListener("click", function() {
+    alert("Well Done!");
+    button16.style.display = 'none';
+});
+const button17 = document.getElementById("Task170");
+
+button17.addEventListener("click", function() {
+    alert("Well Done!");
+    button17.style.display = 'none';
+});
+const button18 = document.getElementById("Task180");
+
+button18.addEventListener("click", function() {
+    alert("Well Done!");
+    button18.style.display = 'none';
+});
+const button19 = document.getElementById("Task190");
+
+button19.addEventListener("click", function() {
+    alert("Well Done!");
+    button19.style.display = 'none';
+});
+const button20 = document.getElementById("Task200");
+
+button20.addEventListener("click", function() {
+    alert("Well Done!");
+    button20.style.display = 'none';
+});
